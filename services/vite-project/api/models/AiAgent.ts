8c44de289ba0813927c8a26e2bb695fc8dce6187
Gleb <romasenkogleb@gmail.com>
2026-02-17 00:18:40 +0300

    delete frontend

---
+++ services/vite-project/api/models/AiAgent.ts
@@ -0,0 +1,15 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type AiAgent = {
+    /**
+     * Уникальный идентификатор агента
+     */
+    id: string;
+    /**
+     * Имя агента
+     */
+    username: string;
+};
+
