dbefd88080d6c25501e3a0e8cd0b79fb79521f78
nikguscode <nikguscode@gmail.com>
2026-02-16 19:12:50 +0500

    123

--- services/ai-agent-service/src/main/java/com/nikguscode/aiagent/agent/application/AgentUsecase.java
+++ services/ai-agent-service/src/main/java/com/nikguscode/aiagent/agent/application/AgentUsecase.java
@@ -1,4 +1,14 @@
 package com.nikguscode.aiagent.agent.application;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class AgentUsecase {
+  public void registerAgent() {
+
+  }
+
+  public void removeAgent() {
+
+  }
 }
