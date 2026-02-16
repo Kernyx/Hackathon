0c874e2ddf88e8ad7135edaa89db6a1a4a8224ab
grush <girois19@gmail.com>
2026-02-16 21:49:10 +0500

    generate /feed from swagger

--- services/go-backend/scripts/generate.sh
+++ services/go-backend/scripts/generate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Генерируем код из openapi.yaml
-oapi-codegen -generate types,server,spec -package openapi ../../openapi/components/audit.yaml > internal/api/openapi/server.gen.go
+~/go/bin/oapi-codegen -generate types,server,spec -package openapi ../../openapi/components/audit.yaml > internal/api/openapi/server.gen.go
 
 echo "Код сгенерирован в internal/api/openapi/server.gen.go"
